1eca52326fa4638004cb4894e512fd77debd7ace
gilzoide <gilzoide@gmail.com>
2023-11-26 10:28:20 -0300

    Add FlexLayoutConfig ScriptableObject

---
+++ Runtime/FlexLayoutConfig.cs
@@ -0,0 +1,54 @@
+using Gilzoide.FlexUi.Yoga;
+using UnityEngine;
+
+namespace Gilzoide.FlexUi
+{
+    [CreateAssetMenu]
+    public class FlexLayoutConfig : ScriptableObject
+    {
+        [SerializeField] private bool _useWebDefaults = false;
+        [SerializeField, Min(0)] private float _pointScaleFactor = 1f;
+        [SerializeField] private Errata _errata = Errata.None;
+
+        [Header("Experimental Features")]
+        [SerializeField] private bool _webFlexBasis = false;
+        [SerializeField] private bool _absolutePercentageAgainstPaddingEdge = false;
+
+        public YGConfig Config
+        {
+            get
+            {
+                if (_config.IsNull)
+                {
+                    _config.Instantiate();
+                    RefreshConfig();
+                }
+                return _config;
+            }
+        }
+
+        private YGConfig _config;
+
+        protected void OnDisable()
+        {
+            _config.Dispose();
+        }
+
+        protected void RefreshConfig()
+        {
+            YGConfig config = Config;
+            config.SetUseWebDefaults(_useWebDefaults);
+            config.SetPointScaleFactor(_pointScaleFactor);
+            config.SetErrata(_errata);
+            config.SetExperimentalFeatureEnabled(ExperimentalFeature.WebFlexBasis, _webFlexBasis);
+            config.SetExperimentalFeatureEnabled(ExperimentalFeature.AbsolutePercentageAgainstPaddingEdge, _absolutePercentageAgainstPaddingEdge);
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            RefreshConfig();
+        }
+#endif
+    }
+}
